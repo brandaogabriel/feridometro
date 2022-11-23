633f7525c03397727d4cbb217b4daf319e2dfce9
Gabriel Brandao <gabriel.brandao@ccc.ufcg.edu.br>
2022-11-23 13:21:20 -0300

    add firebase config

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import './globalStyles.css';
 
 import { ChakraProvider } from '@chakra-ui/react';
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore/lite';
 import { BrowserRouter } from 'react-router-dom';
 
 import { Routes } from './routes';
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const db = getFirestore(app);
 
 function App() {
   return (
